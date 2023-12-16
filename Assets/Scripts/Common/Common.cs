7b86f15d44fca7c3f4ab1eea9ad8bcfec37f7049
MasyoLab <masyo.lab@gmail.com>
2023-12-16 22:05:34 +0900

    更新

---
+++ Assets/Scripts/Common/Common.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MasyoLab.GameFramework.Utils;
+using MasyoLab.GameFramework.Manager;
+using MasyoLab.GameFramework.Input;
+
+namespace MasyoLab.Game
+{
+    public class Common : SingletonMonoBehaviour<Common>
+    {
+        public ResourceManager ResourceManager => ResourceManager.Inst;
+        public ParamManager ParamManager => ParamManager.Inst;
+        public SceneManager SceneManager => SceneManager.Inst;
+        public Gamepad Gamepad => Gamepad.Inst;
+        public Keyboard Keyboard => Keyboard.Inst;
+
+        [RuntimeInitializeOnLoadMethod]
+        static void Initialize()
+        {
+            new GameObject("Common", typeof(Common));
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            gameObject.AddComponent<ResourceManager>();
+            gameObject.AddComponent<ParamManager>();
+            gameObject.AddComponent<SceneManager>();
+            gameObject.AddComponent<Gamepad>();
+            gameObject.AddComponent<Keyboard>();
+        }
+    }
+}
