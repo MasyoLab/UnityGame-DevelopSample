bf01cfc9d3d447376682b12a8e0082ca1a132751
MasyoLab <masyo.lab@gmail.com>
2024-01-08 21:38:49 +0900

    更新

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using MasyoLab.GameFramework.Motion;
+
+namespace MasyoLab.Game
+{
+    public class UnitController : MonoBehaviour
+    {
+        [SerializeField]
+        private MotionManager m_motionManager;
+
+        public void Start()
+        {
+
+        }
+    }
+}
